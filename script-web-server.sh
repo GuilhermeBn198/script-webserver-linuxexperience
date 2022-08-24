4e37f8508ebe111167ab3703baa29013b6ffba9c
Guilherme Bernardo <bguilherme51@gmail.com>
2022-08-24 11:14:40 -0400

    correção script

--- script-web-server.sh
+++ script-web-server.sh
@@ -10,7 +10,7 @@ echo "baixando e copiando os arquivos..."
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
-cd linux-site-dio
+cd linux-site-dio-main
 cp -R * /var/www/html/
 
 echo "tudo configurado!"
